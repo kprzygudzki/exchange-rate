05b369394388f099d6ca1e687d3484ea0677bba3
kprzygudzki <krzysztofprzygudzki@gmail.com>
2017-05-13 23:08:07 +0200

    improved unit test suite for Exchange

--- src/test/java/pl/com/bottega/exchangerate/domain/ExchangeTest.java
+++ src/test/java/pl/com/bottega/exchangerate/domain/ExchangeTest.java
@@ -12,45 +12,119 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ExchangeTest {
 
+	private ExchangeRateBuilder fromExchangeRateBuilder;
+	private ExchangeRateBuilder toExchangeRateBuilder;
+	private BigDecimal amount;
+
 	@Before
 	public void setUp() throws Exception {
-
+		fromExchangeRateBuilder = ExchangeRateBuilder.get()
+				.setCurrency("ABC")
+				.setDate(LocalDate.now())
+				.setRate(BigDecimal.valueOf(4));
+		toExchangeRateBuilder = ExchangeRateBuilder.get()
+				.setCurrency("XYZ")
+				.setDate(LocalDate.now())
+				.setRate(BigDecimal.valueOf(5));
+		amount = BigDecimal.TEN;
 	}
 
 	@Test
 	public void shouldCalculateAmount() {
-		String fromCurrency = "ABC";
-		LocalDate now = LocalDate.now();
-		ExchangeRate fromRate =
-				new ExchangeRate(now, fromCurrency, BigDecimal.valueOf(25).scaleByPowerOfTen(-1));
-		String toCurrency = "XYZ";
-		ExchangeRate toRate =
-				new ExchangeRate(now, toCurrency, BigDecimal.ONE);
-		BigDecimal amount = BigDecimal.valueOf(5);
-		Exchange exchange = Exchange.of(fromRate, toRate, amount, now);
-		BigDecimal expected = BigDecimal.valueOf(1250).scaleByPowerOfTen(-2);
+		//given
+		ExchangeRate fromExchangeRate = fromExchangeRateBuilder.build();
+		ExchangeRate toExchangeRate = toExchangeRateBuilder.build();
+		//when
+		Exchange exchange = Exchange.of(fromExchangeRate, toExchangeRate, amount);
+		//then
 		ExchangeDto exchangeDto = createExchangeDto(exchange);
-		assertEqualExchangeDtos(exchangeDto, fromCurrency, now, toCurrency, amount, expected);
+		assertThat(exchangeDto.getCalculatedAmount()).isEqualByComparingTo(BigDecimal.valueOf(8));
 	}
 
 	@Test
-	public void shouldAssignDateFromFrom() {
-
+	public void shouldCalculateAmountWithDefaultFrom() {
+		//given
+		ExchangeRate toExchangeRate = toExchangeRateBuilder.build();
+		//when
+		Exchange exchange = Exchange.of(ExchangeRate.getDefault(), toExchangeRate, amount);
+		//then
+		ExchangeDto exchangeDto = createExchangeDto(exchange);
+		assertThat(exchangeDto.getCalculatedAmount()).isEqualByComparingTo(BigDecimal.valueOf(2));
 	}
 
-	private void assertEqualExchangeDtos(ExchangeDto exchangeDto, String fromCurrency, LocalDate now, String toCurrency,
-										 BigDecimal amount, BigDecimal expected) {
-		assertThat(exchangeDto.getFrom()).isEqualTo(fromCurrency);
-		assertThat(exchangeDto.getTo()).isEqualTo(toCurrency);
-		assertThat(exchangeDto.getAmount()).isEqualTo(amount);
-		assertThat(exchangeDto.getCalculatedAmount()).isEqualTo(expected);
-		assertThat(exchangeDto.getDate()).isEqualTo(now);
+	@Test
+	public void shouldAssignDateIfToDateIsNull() {
+		//given
+		LocalDate date = LocalDate.parse("2017-07-01");
+		ExchangeRate fromExchangeRate = fromExchangeRateBuilder.setDate(date).build();
+		ExchangeRate toExchangeRate = toExchangeRateBuilder.setDate(null).build();
+		//when
+		Exchange exchange = Exchange.of(fromExchangeRate, toExchangeRate, amount);
+		//then
+		ExchangeDto exchangeDto = createExchangeDto(exchange);
+		assertThat(exchangeDto.getDate()).isEqualTo(date);
+	}
+
+	@Test
+	public void shouldAssignDateIfFromDateIsNull() {
+		//given
+		LocalDate date = LocalDate.parse("2017-07-01");
+		ExchangeRate fromExchangeRate = fromExchangeRateBuilder.setDate(null).build();
+		ExchangeRate toExchangeRate = toExchangeRateBuilder.setDate(date).build();
+		//when
+		Exchange exchange = Exchange.of(fromExchangeRate, toExchangeRate, amount);
+		//then
+		ExchangeDto exchangeDto = createExchangeDto(exchange);
+		assertThat(exchangeDto.getDate()).isEqualTo(date);
+	}
+
+	@Test
+	public void shouldAssignNullDateIfBothDatesAreNull() {
+		//given
+		ExchangeRate fromExchangeRate = ExchangeRate.getDefault();
+		ExchangeRate toExchangeRate = ExchangeRate.getDefault();
+		//when
+		Exchange exchange = Exchange.of(fromExchangeRate, toExchangeRate, BigDecimal.valueOf(5));
+		//then
+		ExchangeDto exchangeDto = createExchangeDto(exchange);
+		assertThat(exchangeDto.getDate()).isNull();
 	}
 
 	private ExchangeDto createExchangeDto(Exchange exchange) {
 		ExchangeDtoBuilder builder = new ExchangeDtoBuilder();
 		exchange.export(builder);
 		return builder.build();
+	}
+
+	private static class ExchangeRateBuilder {
+
+		private String currency;
+		private LocalDate date;
+		private BigDecimal rate;
+
+		private static ExchangeRateBuilder get() {
+			return new ExchangeRateBuilder();
+		}
+
+		private ExchangeRate build() {
+			return new ExchangeRate(date, currency, rate);
+		}
+
+		private ExchangeRateBuilder setCurrency(String currency) {
+			this.currency = currency;
+			return this;
+		}
+
+		private ExchangeRateBuilder setDate(LocalDate date) {
+			this.date = date;
+			return this;
+		}
+
+		private ExchangeRateBuilder setRate(BigDecimal rate) {
+			this.rate = rate;
+			return this;
+		}
+
 	}
 
 }
